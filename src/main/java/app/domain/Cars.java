036642ca74c42a36b7056152c952252a67b5680a
galin <galin-ka1@hotmail.com>
2025-08-06 22:40:58 +0200

    create Cars and Garage Interface

---
+++ src/main/java/app/domain/Cars.java
@@ -0,0 +1,70 @@
+package main.java.app.domain;
+
+import java.util.Objects;
+
+public class Cars {
+   private String regNumber;
+   private String model;
+   private String  company;
+   private double engine;
+    private String color;
+
+    public Cars(String regNumber, String model, String company, double engine, String color) {
+        this.regNumber = regNumber;
+        this.model = model;
+        this.company = company;
+        this.engine = engine;
+        this.color = color;
+    }
+
+    public String getRegNumber() {
+        return regNumber;
+    }
+
+    public void setRegNumber(String regNumber) {
+        this.regNumber = regNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public double getEngine() {
+        return engine;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Cars{");
+        sb.append("regNumber='").append(regNumber).append('\'');
+        sb.append(", model='").append(model).append('\'');
+        sb.append(", company='").append(company).append('\'');
+        sb.append(", engine=").append(engine);
+        sb.append(", color='").append(color).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Cars cars)) return false;
+        return Objects.equals(regNumber, cars.regNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(regNumber);
+    }
+}
---
+++ src/main/java/app/service/Garage.java
@@ -0,0 +1,18 @@
+package main.java.app.service;
+
+public interface Garage {
+
+    boolean addCar(Car car);
+
+    Car removeCar(String regNumber);
+
+    Car findCarsByRegNumber(String regNumber);
+
+    Car[] findCarsByModel(String model);
+
+    Car[] findCarsByCompany(String company);
+
+    Car[] findCarsByEngine(double min, double max);
+
+    Car[] findCarByColor(String color);
+}
